ed97d9fabc8e18a8e3f62dc3f59b6c4c8aa0c3c3
Esu Gabriel <orionesu.dev@gmail.com>
2021-04-10 23:02:57 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+
+    burger.addEventListener('click', () => {
+        //toggle nav 
+        nav.classList.toggle('nav-active');
+    })
+}
+
+navSlide();
